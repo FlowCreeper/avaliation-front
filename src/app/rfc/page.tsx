ff4573cc1ea65034dfd5fc5b110f830cc034a5b8
FlowCreeper <caiocoutinho2012@hotmail.com>
2025-04-27 15:35:05 -0300

    rfc model

--- src/app/rfc/page.tsx
+++ src/app/rfc/page.tsx
@@ -1,7 +1,28 @@
+import { Box, Paper } from "@mui/material";
+
 export default function Rfc() {
   return (
-    <>
-      TODO
-    </>
-  )
+    <Box height="90.9vh" alignContent="center">
+      <Paper sx={{ width: "fit-content", margin: "auto", paddingLeft: 1, paddingRight: 1, background: "rgb(42, 42, 42)", color: "rgb(200,200,200)" }}>
+        <pre>
+          <code>
+{`+---------------------------------------------------------------+
+|                         HTTP/1.1 Message                      |
++---------------------------------------------------------------+
+|                         Start-Line                            |
+|  (Request-Line or Status-Line)                                |
++---------------------------------------------------------------+
+|                         Header Fields                         |
+|  (0 or more header fields)                                    |
++---------------------------------------------------------------+
+|                                                               |
+|                         Message Body                          |
+|  (optional, based on the request or response)                 |
+|                                                               |
++---------------------------------------------------------------+`}
+          </code>
+        </pre>
+      </Paper>
+    </Box>
+  );
 }
